94495db30d3f170de4c96969ca74c63bc476ab99
Siviya Varghese <siviya.varghese.k@gmail.com>
2025-11-05 20:29:07 +0530

    Changes in Utilities Project

---
+++ ClaimBridgeUtilities/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaimBridgeUtilities
+{
+    public class Constants
+    {
+        public const string GetOccupationFactorDetailsException = "Error while retrieving data from GetOccupationFactorDetails";
+        public const string SaveUserDetailsException = "Error while retrieving data from SaveUserDetails";
+    }
+}
---
+++ ClaimBridgeUtilities/Exception/InsuranceException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaimBridgeUtilities.Exceptions
+{
+    public  class InsuranceException:Exception
+    {
+        public InsuranceException()
+        {
+        }
+        public InsuranceException(string message)
+            : base(message)
+        {
+        }
+        public InsuranceException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
